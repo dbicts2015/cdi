e3b01681c8d7c4c42aacd528b2fa21b6d921874c
user104 <user104@192.168.23.118>
2015-07-29 11:57:14 +0200

    met en zonder streams

--- p13cdiweb/src/org/betavzw/hfdstk2/StartServlet.java
+++ p13cdiweb/src/org/betavzw/hfdstk2/StartServlet.java
@@ -22,8 +22,19 @@ public class StartServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		List<String> lijst = databank.getCursussen();
+		
 		if (request.getParameter("filter") != null) {
+			// Met streams
 			lijst = lijst.stream().filter(s -> s.contains((String) request.getParameter("filter"))).collect(Collectors.toList());
+		
+			// Zonder streams
+//			List<String> temp = new ArrayList<>();
+//			for (String string : lijst) {
+//				if (string.contains((String) request.getParameter("filter"))) {
+//					temp.add(string);
+//				}
+//			}
+//			lijst = temp;
 		}
 		request.setAttribute("lijst", lijst);
 		request.getRequestDispatcher("index.jsp").forward(request, response);
